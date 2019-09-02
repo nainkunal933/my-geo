47358deb7ab39600d3f86de2286e361e60e1e4fe
Kunal <azure-admin@m8systems.com>
2019-09-01 17:25:34 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+let base_url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+const api_key = '&key=AIzaSyDRW1vea8xAhj40t9lHQZLotVt34s7WU6g';
+
+
+exports.handler = (event, context, callback) => {
+    const address = event.station;
+    var final_url = base_url + address + api_key;
+    final_url = encodeURI(final_url);
+    var geocode = {};
+    // Sending API request to Google GeoCoding API
+    axios.get(final_url)
+      .then(function (response) {
+        // handle success
+        console.log(response.data.results[0].geometry.bounds.northeast);
+        geocode = {
+          lat: response.data.results[0].geometry.bounds.northeast.lat,
+          lng: response.data.results[0].geometry.bounds.northeast.lng
+        };
+        const result = {
+        statusCode: 200,
+        body: geocode
+      };
+      console.log(result);
+      callback(null, result);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+      
+    
+};
